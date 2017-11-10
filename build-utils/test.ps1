7fc6a2484f15233cc7856af1c0109bc879c808d8
Mike Goldsmith <goldsmith.mike@gmail.com>
2017-11-10 14:04:58 +0000

    use test script and re-add caching

---
+++ build-utils/test.ps1
@@ -0,0 +1,5 @@
+# run unit tests
+dotnet test "tests\Couchbase.Extensions.DependencyInjection.UnitTests"
+dotnet test "tests\Couchbase.Extensions.DnsDiscovery.UnitTests"
+dotnet test "tests\Couchbase.Extensions.Caching.UnitTests"
+dotnet test "tests\Couchbase.Extensions.Session.UnitTests"
